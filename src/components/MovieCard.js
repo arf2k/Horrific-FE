37ffea7644108458235553b414555933381ab99a
https://learn.co/arf2k <foreman.alexander@gmail.com>
2020-10-29 15:27:46 -0400

    updated more style stuff like grids

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,14 +1,33 @@
 import React from 'react';
+import { Card, Grid, GridRow } from 'semantic-ui-react';
+import styled from 'styled-components'
 
 class MovieCard extends React.Component {
 
      render(){
  
           return(
-          <h1>title:{this.props.movie.title} </h1>
+                <Grid container columns={3}>
+                          <Grid.Column>
+                     <Card raised image={`https://image.tmdb.org/t/p/w185${this.props.movie.poster_path}`} />
+                         <Card.Content>
+                              <Card.Header>{this.props.movie.title}</Card.Header>
+                              <Card.Description>{this.props.movie.overview}</Card.Description>
+                         </Card.Content>
+                         </Grid.Column>
+                         </Grid>
+
+            
+           
+            
           )
 
      }
 }
 
 export default MovieCard
+
+const MyCards = styled.div`
+display: list-item,
+
+`
