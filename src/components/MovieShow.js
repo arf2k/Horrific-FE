ee6af2dbfbfaa17a604e1b5e2ac9921bcdcc4c90
https://learn.co/arf2k <foreman.alexander@gmail.com>
2020-10-31 22:40:43 -0400

    started inputting stuff for reviews

--- src/components/MovieShow.js
+++ src/components/MovieShow.js
@@ -1,41 +1,54 @@
 import React from 'react'
-import {Form, TextArea} from 'semantic-ui-react'
+import { Form, TextArea, Button } from 'semantic-ui-react'
 
 
 class MovieShow extends React.Component {
 
 
-
-
-     componentDidMount = () => {
-          let token = localStorage.getItem("token")
-          fetch(`http://localhost:3001/movies/${this.props.chosenMovie}`, {
-               method: "GET",
-               headers:
-                    { Authorization: `Bearer ${token}` }
-          })
-               .then(resp => resp.json())
-               .then(data => {
-                    console.log(data)
-               })
+     state = {
+          review : ""
      }
 
+     // componentDidMount = () => {
+     //      let token = localStorage.getItem("token")
+     //      fetch(`http://localhost:3001${this.props.location.pathname}`, {
+     //           method: "GET",
+     //           headers:
+     //                { Authorization: `Bearer ${token}` }
+     //      })
+     //           .then(resp => resp.json())
+     //           .then(data => {
+     //                console.log(data)
+     //           })
+     // }
 
+     submitReview = () => {
 
+     }
+
+     changeHandler = (e) =>{
+          this.setState({[e.target.name]: e.target.value})
+     }
 
      render() {
-          console.log(this.props.chosenMovie)
+          console.log(this.props)
           return (
                <>
                     <h1>Showpage</h1>
 
-                    <Form>
-                         <TextArea placeholder='Write a review' />
+                    <Form onSubmit={this.submitReview}>
+                         <TextArea placeholder='Write a review' name="review" value={this.state.review} onChange={this.changeHandler} />
+                         <Button
+                              content='Add Review'
+                              labelPosition='right'
+                              icon='pencil'
+                              primary
+                         />
                     </Form>
-)
-
-
                </>
+
+
+
           )
 
      }
